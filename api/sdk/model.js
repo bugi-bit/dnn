256ec435f662fb056ab39a7be5255525302a532e
bugi-bit <85178323+bugi-bit@users.noreply.github.com>
2021-06-20 17:55:21 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -1,14 +1,14 @@
 const tf = require('@tensorflow/tfjs-node');
 
 function normalized(data){ // i & r
-    i = (data[0] - 213.267) / 7.707
-    r = (data[1] - 213.741) / 7.743
+    i = (data[0] - 213.266666666667) / 7.7072300277168
+    r = (data[1] - 213.741666666667) / 7.7438012281854
     return [i, r]
 }
 
 function denormalized(data){
-    v = (data[0] * 0.100) + 0.842
-    p = (data[1] * 7.705) + 213.504
+    v = (data[0] * 0.100312447725892) + 0.842083333333333
+    p = (data[1] * 213.504166666667) + 7.70534668288758
     return [v, p]
 }
 //     p = (data[2] - 0.842) / 0.100
