696dee22f7bb374f42f97e9b7fa44bc87181847d
bugi-bit <85178323+bugi-bit@users.noreply.github.com>
2021-07-04 14:03:57 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,17 +10,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '1825016672:AAFVtA4QKSCzQdRfOz5pYMGYACjl_8xxwS0'
 const bot = new TelegramBot(token, {polling: true});
 
-//router2
-let i = 0;
-let r = 0;
-router.get('/set/:i/:r/', function(req, res, next) {
-  i = req.params.i;
-  r = req.params.r;
-  res.json({
-    i:i, 
-    r:r
-  })
-});
+
 
 state = 0;
 // bots
@@ -31,6 +21,17 @@ bot.onText(/\/start/, (msg) => {
         click /predict`
     );  
     state = 0;
+});
+//router2
+let i = 0;
+let r = 0;
+router.get('/set/:i/:r/', function(req, res, next) {
+  i = req.params.i;
+  r = req.params.r;
+  res.json({
+    i:i, 
+    r:r
+  })
 });
 
 bot.onText(/\/monitor/, (msg) => { 
